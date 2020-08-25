3eeb311b526c4c251ef19f8f5cd443033e747c59
rafliID <rafolufofficial@gmail.com>
2020-08-25 10:36:01 +0700

    action back at actionbar

--- app/src/main/java/can/co/id/myapp/SecondActivity.kt
+++ app/src/main/java/can/co/id/myapp/SecondActivity.kt
@@ -2,10 +2,20 @@ package can.co.id.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
